51d14b7798341b094d3c3cc6144f706ae7c87eab
lajos <lj@yunpankeji.com>
2020-04-17 11:54:04 +0800

    修改配置文件，默认火狐驱动

--- cookiespool/config.py
+++ cookiespool/config.py
@@ -5,14 +5,15 @@ REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 
 # Redis密码，如无填None
-REDIS_PASSWORD = 'foobared'
+REDIS_PASSWORD = None
 
 # 产生器使用的浏览器
-BROWSER_TYPE = 'Chrome'
+BROWSER_TYPE = 'Firefox'
 
 # 产生器类，如扩展其他站点，请在此配置
 GENERATOR_MAP = {
-    'weibo': 'WeiboCookiesGenerator'
+    'weibo': 'WeiboCookiesGenerator',
+    # 'zhihu': 'ZhihuCookiesGenerator'
 }
 
 # 测试类，如扩展其他站点，请在此配置
@@ -32,7 +33,7 @@ API_HOST = '0.0.0.0'
 API_PORT = 5000
 
 # 产生器开关，模拟登录添加Cookies
-GENERATOR_PROCESS = False
+GENERATOR_PROCESS = True
 # 验证器开关，循环检测数据库中Cookies是否可用，不可用删除
 VALID_PROCESS = False
 # API接口服务
